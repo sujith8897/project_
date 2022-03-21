9e65567408d77df5a965f9d7f73fee5bd5e054d4
sujith8897 <sujithkumar8897@gmail.com>
2022-03-21 09:00:30 +0530

    first commit

---
+++ src/Main/index.js
@@ -0,0 +1,41 @@
+import './style.css';
+
+export default function Main() {
+    return (
+        <>
+            <div class='main'>
+                <div class='left'>
+                    <div class='content'>
+                        <h1>Stable & Profitable Yield Farming Dapp With Automated Users Insurance</h1>
+                        <p>From 10% up to 20% Daily ROI 5 Levels of Referral Rewards</p>
+                        <div class='buttons'>
+                            <button id='red-btn'>Deposit</button>
+                            <button>Telegram</button>
+                            <button>Audit</button>
+                            <button>Contract</button>
+                        </div>
+                    </div>
+                </div>
+                <img src='locker.webp' />
+            </div>
+
+            <div class='list'>
+                <div>
+                    <h3>Total Staked Volume</h3>
+                    <p>3,483.72 Matic</p>
+                    <p>$ 5,260.41</p>
+                </div>
+                <div>
+                    <h3>Total Staked Volume</h3>
+                    <p>3,483.72 Matic</p>
+                    <p>$ 5,260.41</p>
+                </div>
+                <div>
+                    <h3>Total Staked Volume</h3>
+                    <p>3,483.72 Matic</p>
+                    <p>$ 5,260.41</p>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/Navbar/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+export default function Navbar() {
+    return(
+        <div class="navbar">
+            <p class="logo">Logo</p>
+            <button class="btn">CONNECT WALLET</button>
+        </div>
+    );
+}
